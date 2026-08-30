01ca71b557638b5e1a45ee497d1e0f633e548387
so2 <so2jin5@gmail.com>
2026-08-30 16:44:59 +0900

    マスターデータの型を微調整

--- src/types/trusttracker.ts
+++ src/types/trusttracker.ts
@@ -14,12 +14,26 @@
  */
 export type TrustCombatType =
 	| '近接物理'
-	| '遠距離物理'
+	| '遠隔物理'
 	| '魔法攻撃'
 	| '回復'
 	| '支援'
 	| '盾'
 	| string;
+
+/**
+ * フェイスの所属分類
+ */
+export type TrustAffiliation =
+	| 'サンドリア'   // 赤色
+	| 'バストゥーク' // 青色
+	| 'ウィンダス'   // 緑色
+	| 'ジュノ'       // 白色
+	| 'プロマシア'   // 橙色
+	| 'アトルガン'   // 紫色
+	| 'アルタナ'     // 真紅
+	| 'アドゥリン'   // 青緑
+	| 'その他'       // 灰色
 
 /**
  * 盟（アイテム）情報
@@ -43,6 +57,7 @@ export type TrustMaster = {
 	party_name: string;
 	job: string;
 	combatType: TrustCombatType;
+	affiliation?: TrustAffiliation;
 	isLimited: boolean;
 	acquireInfo: string;
 	item: TrustItemInfo;
